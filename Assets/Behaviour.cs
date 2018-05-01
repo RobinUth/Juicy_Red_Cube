285d103f838bf21cb3b3b636ba1940011666f59d
RobinUth <robin.uth95@gmail.com>
2018-05-01 19:24:11 +0200

    Initial commit

---
+++ Assets/Behaviour.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Behaviour : MonoBehaviour {
+
+
+
+
+    [Range(1, 10)]
+    [SerializeField] float jumpVelocity;
+    [SerializeField] int normalSize = 1;
+    [SerializeField] float scalingX = 0.1f;
+    [SerializeField] float scalingY = 0.1f;
+    [SerializeField] float scalingZ = 0.1f;
+    bool size = true;
+    Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+
+        Jump();
+
+
+        if (transform.localScale.y <= 0.1f || transform.localScale.x <= 0.1f)
+        {
+            size = false;
+        }
+
+
+        if (size)
+        {
+           // SquashStrech();
+        }
+       if(Input.GetKey(KeyCode.R))
+        {
+            transform.localScale = new Vector3(normalSize, normalSize, normalSize);
+            size = true;
+        }
+	}
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            rigidbody.velocity = Vector3.up * jumpVelocity;
+
+
+        }
+    }
+    
+    void SquashStrech()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            transform.localScale += new Vector3(scalingX, -scalingY, scalingZ );
+            
+        }
+        if(Input.GetKey(KeyCode.F))
+        {
+            transform.localScale -= new Vector3(scalingX, -scalingY, scalingZ);
+        }
+    }
+}
---
+++ Assets/BetterJump.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetterJump : MonoBehaviour {
+
+    [SerializeField] float fallMultiplier = 2.5f;
+    [SerializeField] float lowJumpMultiplier = 2f;
+
+    Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if(rigidbody.velocity.y < 0)
+        {
+            rigidbody.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier-1) * Time.deltaTime;
+        }else if(rigidbody.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rigidbody.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+        
+    }
+}
